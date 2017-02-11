28a4f893f62250f333b3c3f97efa31e8cfbfb939
wolfson <wolfson@bracketedrebels.com>
2017-02-12 00:37:28 +0700

    query executor class added

---
+++ src/queries/executor.class.ts
@@ -0,0 +1,65 @@
+import { Entity } from '../entity.class';
+import { TagClass } from '../manager.interfaces';
+
+import { Query } from './query.class';
+import { QueryTypes } from './query.enums';
+import { andExecutor, setInvertor, orExecutor, setsIntersector, setsUnioner } from './executor.helpers';
+
+
+
+export class QueryExecutor {
+    constructor( private entities: Entity[] ) {}
+
+    public execute( query: Query ): Entity[] {
+        return this.executeRecursive(query, this.entities);
+    }
+
+
+
+    private mutation: Entity[];
+
+    private executeRecursive(query: Query, ents: Entity[]): Entity[] {
+        let lTags = [];
+        let lSubqueries = [];
+        let lType = query.type;
+        query.args.forEach( arg => (arg instanceof Query ? lSubqueries : lTags).push(arg) );
+        let lEntitesSets = [];
+        if (lTags.length) {
+            lEntitesSets.push(this.proceedTags(lType, lTags, ents));
+        }
+        if (lSubqueries) {
+            lEntitesSets = lEntitesSets.concat(this.proceedSubqueries(lSubqueries, ents));
+        }
+        return this.combineEntitiesSets(lType, lEntitesSets);
+    }
+
+    private proceedTags(type: QueryTypes, tags: TagClass<any>[], ents: Entity[]): Entity[] {
+        switch(type) {
+            case QueryTypes.or:
+                return orExecutor(tags, ents);
+            case QueryTypes.and:
+                return andExecutor(tags, ents);
+            case QueryTypes.not:
+                return setInvertor(ents, this.entities);
+        }
+    }
+
+    private proceedSubqueries(queries: Query[], ents: Entity[]): Entity[][] {
+        return queries.reduce( (acc, v) => {
+            acc.push( this.executeRecursive(v, ents) );
+            return acc;
+        }, [] );
+    }
+
+    private combineEntitiesSets(type: QueryTypes, entSets: Entity[][]): Entity[] {
+        switch(type) {
+            case QueryTypes.or:
+                return setsUnioner(entSets);
+            case QueryTypes.and:
+                return setsIntersector(entSets);
+            case QueryTypes.not:
+                // assuming that query is valid and consume only one argument
+                return setInvertor(entSets[0], this.entities);
+        }
+    }
+}
---
+++ src/queries/executor.helpers.ts
@@ -0,0 +1,27 @@
+import * as _ from 'underscore';
+
+import { Entity } from '../entity.class';
+import { TagClass } from '../manager.interfaces';
+
+
+
+
+export function andExecutor( tags: TagClass<any>[], ents: Entity[] ): Entity[] {
+    return ents.filter( e => e.markedWith(tags, 'and'));
+}
+
+export function orExecutor( tags: TagClass<any>[], ents: Entity[] ): Entity[] {
+    return ents.filter( e => e.markedWith(tags, 'or'));
+}
+
+export function setInvertor( ents: Entity[], whole: Entity[] ): Entity[] {
+    return whole.filter( e => ents.indexOf(e) < 0 );
+}
+
+export function setsUnioner<T>( sets: T[][] ): T[] {
+    return _.union.apply(_, sets);
+}
+
+export function setsIntersector<T>( sets: T[][] ): T[] {
+    return _.intersection.apply(_, sets);
+}

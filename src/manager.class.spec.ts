80e665c4de0e314afbe439207f36908c1f331199
wolfson <wolfson@bracketedrebels.com>
2017-04-10 23:26:50 +0700

    fix: manager integrated with new tagging process
    BREAKING CHANGE: createTag API removed from manager API due to new tagging process implemented.

--- src/manager.class.spec.ts
+++ src/manager.class.spec.ts
@@ -3,9 +3,6 @@ import 'jasmine';
 import { Entity } from './entity.class';
 import { Manager } from './manager.class';
 import { ALL } from './query.helpers';
-import { Tag } from './tag.class';
-
-
 
 describe(`Manager`, () => {
     const manager = new Manager();
@@ -14,18 +11,11 @@ describe(`Manager`, () => {
         expect(manager).toBeTruthy();
     });
     it(`should correctly create entities`, () => {
-        expect(manager.createEntity() instanceof Entity).toBeTruthy();
+        expect(manager.create() instanceof Entity).toBeTruthy();
     });
     it(`should correctly accumulate created entities`, () => {
-        let entities = manager.queryEntities(ALL);
-        entities.push(manager.createEntity());
-        expect(manager.queryEntities(ALL).sort()).toEqual(entities.sort());
-    });
-    it(`should correctly create tags`, () => {
-        let TAG = Manager.createTag(() => 1);
-        let lTagInstance = new TAG();
-        expect(lTagInstance instanceof TAG).toBeTruthy();
-        expect(lTagInstance instanceof Tag).toBeTruthy();
-        expect(lTagInstance.value).toBe(1);
+        let entities = manager.query(ALL);
+        entities.push(manager.create());
+        expect(manager.query(ALL).sort()).toEqual(entities.sort());
     });
 });
--- src/manager.class.ts
+++ src/manager.class.ts
@@ -1,26 +1,11 @@
-import { TagClass } from './manager.interfaces';
-import { Tag } from './tag.class';
+import { Class } from './tag.interfaces';
 import { Entity } from './entity.class';
 import { QueryExecutor } from './executor.class';
 import { Query } from './query.class';
 
-import { uniqueId } from 'lodash';
-
 
 
 export class Manager {
-    /**
-     * Creates tag for tagging entites and querying them. Tag - is a class, taking
-     * tag value as an argument when instancing.
-     * @argument defaultValueMaker is a handler producing value that will be used
-     *           when no tag value provided while instancing. If no default value
-     *           and no handler provided, undefined will be set as a tag value.
-     * @returns Tag class that can be instanced for entity tagging.
-     */
-    public static createTag<T>(defaultValueMaker?: () => T): TagClass<T> {
-        return this.createTagClass(defaultValueMaker);
-    }
-
     /**
      * Queries entites via boolean opertaors over the tags. Entities are taken
      * from the Manager local storage which is unique for every Manager instance.
@@ -28,7 +13,7 @@ export class Manager {
      * @argument query - boolean query over the tags, created via createTag method.
      * @returns list of entities that satisfied provided query.
      */
-    public queryEntities(query: Query | TagClass<any>): Entity[] {
+    public query(query: Query | Class<any>): Entity[] {
         return this.executor.execute(query);
     }
 
@@ -37,29 +22,12 @@ export class Manager {
      * for futher querying.
      * @returns recently created entity.
      */
-    public createEntity(): Entity {
-        return this.entities[this.entities.push(new Entity(uniqueId('ent'))) - 1];
+    public create(): Entity {
+        return this.entities[this.entities.push(new Entity()) - 1];
     }
-
 
 
 
     private entities: Entity[] = [];
     private executor = new QueryExecutor(this.entities);
-
-    private static createTagClass<T>(defaultValueMaker?: () => T): TagClass<T> {
-        let lIdentifier = uniqueId('tag');
-        return this.generateTagClass(lIdentifier, defaultValueMaker);
-    }
-
-    private static generateTagClass<T>(identifier: string, defaultValueMaker?: () => T): TagClass<T> {
-        return class extends Tag<T> {
-            public static toString(): string { return identifier; }
-            constructor( value?: T ) { super( identifier,
-                arguments.length === 0 
-                    ? defaultValueMaker && defaultValueMaker() || undefined
-                    : value
-            ); }
-        };
-    }
 }

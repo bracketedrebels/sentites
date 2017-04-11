62bc6af25c96cf16ef0b33523683a5a2fca93d3e
wolfson <wolfson@bracketedrebels.com>
2017-04-11 22:45:12 +0700

    fix: exporting tag decorator

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Manager as Sentities } from './manager.class';
 export { Entity } from './entity.interfaces';
+export { Tag } from './tag.decorator';
 export { ALL, AND, NONE, NOT, OR } from './query.helpers';
